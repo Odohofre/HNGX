d415d2359a6664602c8ff41441e86d76756cc151
Odohofre <odohofrebright@gmail.com>
2023-09-19 18:45:53 +0100

    fix: remove unused import

--- stage-3/src/App.jsx
+++ stage-3/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import Auth from './pages/Auth';
 
